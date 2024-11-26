ead406d00a5d13eee55073bd48808cf7b25ea079
np-allane <144137504+np-allane@users.noreply.github.com>
2024-11-26 23:37:52 +0200

    credentials setup fix

--- ui-tests/config/creedentials.ts
+++ ui-tests/config/creedentials.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const credentials = {
-	username: process.env.ADMIN_USERNAME,
-	password: process.env.ADMIN_PASSWORD
+	username: process.env.ADMIN_USERNAME || Cypress.env('ADMIN_USERNAME'),
+	password: process.env.ADMIN_PASSWORD || Cypress.env('ADMIN_PASSWORD')
 };
