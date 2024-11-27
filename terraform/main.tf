c371b5b1792ce1949d9ef02a98ddccb72c01fd8a
np-allane <144137504+np-allane@users.noreply.github.com>
2024-11-27 13:18:50 +0200

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,13 +34,3 @@ resource "google_compute_instance" "default" {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
-
-output "instance_ip" {
-  description = "The public IP address of the VM."
-  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-}
-
-output "instance_name" {
-  description = "The name of the VM instance."
-  value       = google_compute_instance.default.name
-}
