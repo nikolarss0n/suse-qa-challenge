346d3472e7c04f679091335f0f87affebc0a06ce
np-allane <144137504+np-allane@users.noreply.github.com>
2024-11-27 15:21:18 +0200

    wip

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(var.credentials_file)
+  credentials = var.credentials_file != "" ? file(var.credentials_file) : null
   project     = var.project
   region      = var.region
 }
