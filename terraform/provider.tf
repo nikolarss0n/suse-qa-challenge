6b9a0897bdde672866658f6ed43731899ea47aa6
np-allane <144137504+np-allane@users.noreply.github.com>
2024-11-27 16:08:05 +0200

    wip

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = null # Use null since GOOGLE_CREDENTIALS env variable will be used
-  project     = var.project
-  region      = var.region
+  project = var.project
+  region  = var.region
 }
