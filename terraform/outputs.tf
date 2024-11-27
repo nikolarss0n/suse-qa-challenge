511c9f754ae752bc06bfa2b93821420d419a64db
Your Second Account Name <n.prosenikov@gmail.com>
2024-11-27 21:12:41 +0200

    wip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,10 @@
 output "instance_ip" {
-  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+  description = "The public IP address of the instance"
+  value       = trim(google_compute_instance.default.network_interface[0].access_config[0].nat_ip, " ")
+  sensitive   = false
 }
 
-
 output "instance_name" {
-  description = "The name of the VM instance."
+  description = "The name of the instance"
   value       = google_compute_instance.default.name
 }
