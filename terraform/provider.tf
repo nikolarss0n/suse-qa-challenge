2ed59ec3c7fffbe8c9616a13a3425e84ae1c25ed
np-allane <144137504+np-allane@users.noreply.github.com>
2024-11-27 16:28:34 +0200

    wip

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,8 @@
 provider "google" {
   project = var.project
   region  = var.region
+  zone    = var.zone
+
+  # Use the GitHub Actions public key for authentication
+  access_token = file("github_actions_key.pub")
 }
