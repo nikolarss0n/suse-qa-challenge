54c2623645bf966038beeebbe2831b1309ba952d
nklars0 <n.prosenikov@gmail.com>
2024-11-27 16:48:28 +0200

    Fix Terraform Permissions Issue for Compute Engine Deployment Summary (#4)
    * terraform

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "default" {
+  name         = "test-vm"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = var.tags
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image_project}/${var.image_family}"
+    }
+  }
+
+  network_interface {
+    network    = var.network
+    subnetwork = var.subnetwork != "" ? var.subnetwork : null
+
+    access_config {
+      # Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    echo "Hello, World!" > /var/www/html/index.html
+  EOT
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("${path.module}/github_actions_key.pub")}"
+  }
+
+  service_account {
+    email  = "terraform-deployer@${var.project}.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,57 @@
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "image_family" {
+  description = "Image family to use for the VM"
+  type        = string
+  default     = "debian-11"
+}
+
+variable "image_project" {
+  description = "Image project to use for the VM"
+  type        = string
+  default     = "debian-cloud"
+}
+
+variable "network" {
+  description = "VPC network name"
+  type        = string
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "Subnetwork name"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Network tags for the VM"
+  type        = list(string)
+  default     = []
+}
+
+variable "credentials_file" {
+  description = "Path to the GCP service account credentials file"
+  type        = string
+}
